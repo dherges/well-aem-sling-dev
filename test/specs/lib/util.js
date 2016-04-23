a0adf24b1c62c7055d432b3200e59874b42ef233
dherges <david@spektrakel.de>
2016-04-23 21:51:29 +0200

    fix up first class error

--- test/specs/lib/util.js
+++ test/specs/lib/util.js
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2016 David Herges. https://github.com/dherges/aem-sling-dev-well
+ * Copyright (c) 2016 David Herges. https://github.com/dherges/well-aem-sling-dev
  *
  * Licensed under the Apache License 2.0.
  * http://www.apache.org/licenses/LICENSE-2.0
@@ -13,7 +13,7 @@ var assert = require('chai').assert
 
 describe('util', function() {
 
-  describe('extractContentPath()', function () {
+  describe('.extractContentPath()', function () {
     it('should return path relative to "jcr_root"', function () {
       expect(util.extractContentPath('jcr_root/tmp/blubbfile.js')).to.equal('tmp/blubbfile.js')
     })
@@ -29,7 +29,7 @@ describe('util', function() {
 
 
 
-  describe('extractJcrRootPath()', function () {
+  describe('.extractJcrRootPath()', function () {
     it('should return path relative to "jcr_root"', function () {
       expect(util.extractJcrRootPath('jcr_root/tmp/blubbfile.js')).to.equal('jcr_root')
     })
@@ -40,6 +40,21 @@ describe('util', function() {
 
     it('should return falsy if no "jcr_root" found', function () {
       expect(util.extractJcrRootPath('fooo')).to.be.undefined
+    })
+  })
+
+
+  describe('.resolveContentPath()', function () {
+    it('should resolve a path relative to "jcr_root"', function () {
+      expect(util.resolveContentPath('jcr_root', 'jcr_root/tmp/blubbfile.js')).to.equal('tmp/blubbfile.js')
+    })
+
+    it('should resolve a path relative to "jcr_root" for absolute paths', function () {
+      expect(util.resolveContentPath('/abspath/jcr_root/', '/abspath/jcr_root/tmp/blubbfile.js')).to.equal('tmp/blubbfile.js')
+    })
+
+    it('should return falsy if no "jcr_root" found', function () {
+      expect(util.resolveContentPath('fooo', 'bar')).to.be.false
     })
   })
 
